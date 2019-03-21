d83c382dc2a6274cfab42140c767d92e238fc438
Matteo Fratesi <matteo.fratesi@openmindonline.it>
2019-03-21 16:06:50 +0100

    webpack first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import './style.css'; 
+
+function component() {
+  console.log('hello webpack');
+}
+
+function testLodash() {
+  let element = document.createElement('div');
+
+   // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(testLodash());
+component();
