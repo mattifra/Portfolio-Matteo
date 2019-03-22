b035c0ff9bdb6362a937cab039ce4ff45ea798fa
Matteo Fratesi <matteo.fratesi@openmindonline.it>
2019-03-22 17:43:11 +0100

    publicpath

--- webpack.common.js
+++ webpack.common.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
+    publicPath: '/'
   },
   plugins: [
     new CleanWebpackPlugin(),
