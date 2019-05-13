e3ae2dc6bed9b230f669891779963790becd923d
mattifra <matteo@lacerba.io>
2019-05-13 14:52:01 +0200

    images as webp

--- webpack.common.js
+++ webpack.common.js
@@ -62,7 +62,7 @@ module.exports = {
         ]
       }, 
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|webp)$/,
          use: {
           loader: 'file-loader',
           options: {
